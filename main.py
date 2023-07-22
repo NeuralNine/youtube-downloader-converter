fad64019ed51609d178e8ceadebe6e08af954cf1
Meit Sant <meitsant@gmail.com>
2023-07-22 22:42:23 +0530

    Combined improvements from past pull requests + Added some of mine
    Merge Notes
    - Added Comments
    - Added requirements.txt
    - Added a neat little "Info" comment with names of contributors
    - After conversion to MP3, original MP4 file is deleted
    - All downloads are now saved to a 'Downloaded' Folder in the same dir as the program, to avoid clutter while downloading a playlist
    - Added a progress bar for video downloading.
    - Speed improvements : Removed wildcard import of pytube
    
    Co-Authored-By: daniel rojas <8826407+l20j45@users.noreply.github.com>
    Co-Authored-By: Nouman Rahman <nouman.rahman2010@gmail.com>
    Co-Authored-By: Aswin Lal M <aswnss.m@gmail.com>

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
-print("Welcome to NeuralNine YouTube Downloader and Converter v0.2 Alpha")
-print("Loading...")
+#-------------------------------------------------------------------------------
+# Name:         YouTube Downloader and Converter v0.3
+#
+# Author:       NeuralNine
+#
+# Created:      2020
+# Copyright:    Free Use License
+#
+# Lead Dev :    NeuralNine
+# Improvements: MT_276 [https://github.com/MT-276]
+#               l20j45 [https://github.com/l20j45]
+#               ProgrammingFire [https://github.com/ProgrammingFire]
+#               aswnss-m [https://github.com/aswnss-m]
+#-------------------------------------------------------------------------------
 
-import pytube
-import youtube_downloader
-import file_converter
+print("Welcome to NeuralNine YouTube Downloader and Converter v0.3\n")
+print("Loading modules...")
 
+from youtube_downloader import download_playlist,input_links,download_video,convert_to_mp3
+import os
+
+print("Load Complete\n")
 print('''
-What do you want?
+Options
 
 (1) Download YouTube Videos Manually
 (2) Download a YouTube Playlist
 (3) Download YouTube Videos and Convert Into MP3
 
 Downloading copyrighted YouTube videos is illegal!
-I am not responsible for your downloads! Go at your own risk!
+We are not responsible for your downloads! Go at your own risk!
 
-Copyright (c) NeuralNine 2020
+Copyright - Free Use license\n
 ''')
 
+#Asks user for choice
 choice = input("Choice: ")
 
 if choice == "1" or choice == "2":
-    quality = input("Please choose a quality (low, medium, high, very high):")
-    if choice == "2":
+
+    # Sets Quality Option of video(s)
+    quality = input("Please choose a quality (low or 0, medium or 1, high or 2, very high or 3):")
+
+    if choice == "1": # Download YouTube Videos Manually
+        links = input_links()
+        print('Download has been started')
+        for link in links: download_video(link, quality)
+        print("Download finished!")
+
+    if choice == "2": # Download a YouTube Playlist
         link = input("Enter the link to the playlist: ")
         print("Downloading playlist...")
-        youtube_downloader.download_playlist(link, quality)
+        download_playlist(link, quality)
         print("Download finished!")
-    if choice == "1":
-        links = youtube_downloader.input_links()
-        for link in links:
-            youtube_downloader.download_video(link, quality)
-elif choice == "3":
-    links = youtube_downloader.input_links()
+
+elif choice == "3": # Download YouTube Videos and Convert Into MP3
+    links = input_links()
     for link in links:
         print("Downloading...")
-        filename = youtube_downloader.download_video(link, 'low')
+        filename = download_video(link, 'low')
         print("Converting...")
-        file_converter.convert_to_mp3(filename)
+        convert_to_mp3(filename)
+        os.remove(filename)
 else:
+    # If input is anything other than 1,2 or 3. The program will exit.
     print("Invalid input! Terminating...")
+    exit()
